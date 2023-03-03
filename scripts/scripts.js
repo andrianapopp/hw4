91a30de53857f3c396b0064d29fee15c1848c6d7
Andriana Popp <115152248+andrianapopp@users.noreply.github.com>
2023-03-03 20:06:09 +0200

    else if

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,24 +1,20 @@
-let x = prompt(`first_number`)
-let y = prompt(`second_umber`)
-let action = prompt(`Choose an action: sum, subtraction, multiplication, division`)
+const x = prompt(`first_number`)
+const y = prompt(`second_umber`)
+const action = prompt(`Choose an action: sum, subtraction, multiplication, division`)
 
 if (action === `sum`){
     let z = parseFloat(x) + parseFloat(y)
     alert(`Результат: ${x} + ${y}=${z}`)
 }
-else
-    if (action === `subtraction`){
+else if (action === `subtraction`){
     let z = x - y
     alert(`Результат: ${x} - ${y}=${z}`)
 }
-else
-    if (action === `multiplication`){
+else if (action === `multiplication`){
     let z = x * y
     alert(`Результат: ${x} * ${y}=${z}`)
 }
-else
-    if (action === `division`){
+else if (action === `division`){
     let z = x / y
     alert(`Результат: ${x} / ${y}=${z}`)
 }
-    else;
